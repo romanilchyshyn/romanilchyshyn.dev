a96396fd7ed32b49ea4967e06bd8b628dd7acc67
Roman Ilchyshyn <romanilchyshyn@gmail.com>
2025-07-06 18:02:14 +0300

    cp sitemap.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 mkdir -p public
 
 cp style.css public/style.css
+cp sitemap.xml public/sitemap.xml
 
 pandoc index.md -o public/index.html --to=html5 --template=template.html --metadata title="romanilchyshyn.dev"
 
